f81111aa294c0b7988b7b063bfe4697e4faac6b4
John Caldwell <caldwelljs@gmail.com>
2013-12-07 15:49:52 -0600

    create user and car model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131207213729) do
+ActiveRecord::Schema.define(version: 20131207214835) do
+
+  create_table "cars", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "seats"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"

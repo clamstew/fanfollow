daedb9fc941b125cca00db91f964d5127bcccdd6
Andy Murphy <mrajmurphy@gmail.com>
2013-12-07 13:42:43 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fanfollow::Application.config.secret_key_base = '95018c668a0c26f4e2cc2c2e0041eb0a2d6fbb9799a3868f0e3120442712a588dadcf1e8acac066119d3410fabd71974d903e84754ad16676dddbc72d9637284'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fanfollow::Application.config.session_store :cookie_store, key: '_fanfollow_session'

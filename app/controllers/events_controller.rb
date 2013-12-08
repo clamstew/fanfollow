8eb1086630cdb4726febf57db266fa4f3ea55cc9
John Caldwell <caldwelljs@gmail.com>
2013-12-07 20:30:46 -0600

    create event_controller.rb with CRUD actions

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,46 @@
+class EventsController < ApplicationController
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    # If you want to store who created this event, write @event.user_id = current_user.id
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Video.find(params[:id])
+    @video.update(video_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :description, :image_id, :date, :location)
+  end
+
+end
